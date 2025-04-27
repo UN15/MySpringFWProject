f1645c6c623f25d6368500671b549d9de01d4ac2
정재운 <jaeun101501@gmail.com>
2025-04-27 14:31:57 +0900

    springframework di 연습문제 실습1-1, 실습1-2 추가

---
+++ src/test/java/mylab/order/di/xml/OrderSpringTest.java
@@ -0,0 +1,42 @@
+package mylab.order.di.xml;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:mylab-order-di.xml")
+public class OrderSpringTest {
+	@Autowired
+	private ShoppingCart cart;
+	
+	@Autowired
+	private OrderService service;
+	
+	@Test
+	void testShoppingCart() {
+		assertNotNull(cart);
+		System.out.println("testShoppingCart Test");
+		assertEquals(2, cart.getProducts().size());
+		assertEquals(8000, cart.getTotalPrice());
+		assertEquals("Apple",cart.getProducts().get(0).getName());
+		System.out.println(cart.toString());
+	}
+	
+	@Test
+	void testOrderService() {
+		assertNotNull(service);
+		System.out.println("testOrderService Test");
+		assertEquals(8000, service.calculateOrderTotal());
+		System.out.println(service.toString());
+		for(Product product: service.getShoppingCart().getProducts()) {
+			System.out.println(product);
+		}
+	}
+}
--- src/test/java/myspring/di/xml/HelloJunitTest.java
+++ src/test/java/myspring/di/xml/HelloJunitTest.java
@@ -1,6 +1,7 @@
 package myspring.di.xml;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -9,17 +10,31 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class HelloJunitTest {
+	ApplicationContext context;
+
+	@BeforeEach
+	void container(){
+		//1.Container 객체 생성
+		 context = new GenericXmlApplicationContext("classpath:hello-di.xml");			
+	}
 	@Test
 	void helloBean() {
-		//1.Container 객체 생성
-		ApplicationContext context = new GenericXmlApplicationContext("classpath:hello-di.xml");
-		//2.Container 객체가 생성한 Spring Bean을 요청하기
+		//2.Container 객체가 생성한 Hello Spring Bean을 요청하기
 		Hello helloById = (Hello) context.getBean("hello");
 		Hello helloByType = context.getBean("hello", Hello.class);
+		
 		//Spring Bean의 주소 비교
 		System.out.println(helloById == helloByType); //scope default는 singleton prototype으로 변경해주면 주소 달라짐
 		assertSame(helloById, helloByType);
 		
+		//값 비교
+		assertEquals("Hello 스프링", helloById.sayHello());
+		
+		helloById.print();
+		//Container 객체가 생성한 StringPrinter 스프링빈을 요청하기
+		//<property name ="printer" ref = "strPrinter" /> 이 설정을 테스트 한 것임
+		Printer printer = context.getBean("strPrinter", Printer.class);
+		assertEquals("Hello 스프링", printer.toString());
 	}
 
 	
